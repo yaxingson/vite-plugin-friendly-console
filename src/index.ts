a8003442df9ce22c4d8b6e45e023d03b71115011
soetas <3228891558@qq.com>
2025-03-18 22:40:28 +0800

    feat: init

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,6 @@
+import { resolve } from 'node:path'
+import { performance } from 'node:perf_hooks'
+import { stat } from 'node:fs/promises'
 import type { PluginOption } from 'vite'
 
 interface FriendlyConsoleOption {
@@ -10,19 +13,45 @@ export default function friendlyConsolePlugin(option:FriendlyConsoleOption={}): 
     clearConsole = true,
   } = option
 
-  console.log(option)
+  let start = 0
+  let outDir = ''
+  let bundlePath = ''
 
   return {
     name:'vite-plugin-friendly-console',
+    enforce:'pre',
     apply:'build',
+    config(config, env) {
+      outDir = config.build.outDir || 'dist'
+    },
+    options(options) {
+      options.plugins[0]['buildStart'] = ()=>{
+        start = performance.now()
+      }
+      options.plugins[(options.plugins as any[]).length - 1]['closeBundle'] = ()=>{
+        console.log(performance.now() - start)
+      }
+    },
     buildStart(options) {
       
     },
-    buildEnd(error) {
+    resolveId(source, importer,{ isEntry }) {
       
     },
-    closeBundle(error) {
-      // console.clear()
+    buildEnd(error) {
+    
+    },
+    generateBundle(options, bundle, isWrite) {
+      for(const path in bundle) {
+        bundlePath = resolve(outDir, path)
+      }
+    },
+    async closeBundle(error) {
+      console.log((await stat(bundlePath)).size)
+    },
+    onLog(level, log) {
+      this.info(level)
+      return false
     }
   }
 }
